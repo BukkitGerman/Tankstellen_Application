89b33501c6ed58b2ea28bf652fe241196573e50f
BukkitGerman <preussjustin@outlook.de>
2019-10-14 23:27:41 +0200

    added a loop script to get more data over time while executing.

---
+++ loop.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Loop</title>
+</head>
+<body>
+
+<style type="text/css">
+	button{
+		background-color: gray;
+		border-radius: 4px;
+	}
+</style>
+<button onclick="loop()">Start loop!</button>
+<p id="data"></p>
+<script type="text/javascript">
+		
+	function loop() {
+		let currentdate = new Date();
+		let time = currentdate.getMinutes();
+		let arr = [];
+		console.log("Start record!")
+		setInterval(function(){
+			document.getElementById("data").innerHTML = "Last Refresh at: " + time;
+			console.log("Tracked new Data.")
+			fetch('./data.php?anzahl=12')
+			.then(response => response.json())
+			.then(data => {
+				data["stations"].forEach(station => {
+					arr.push(station)
+				})
+				console.log(arr)
+			})
+		}, 300000);
+	}
+
+
+</script>
+
+</body>
+</html>
+
